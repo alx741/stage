b5ecb19cce6c4f3c3b98420ed7f9960e92a312cf
Daniel Campoverde <alx@sillybytes.net>
2017-07-26 22:26:55 -0500

    General framework

--- stage.sh
+++ stage.sh
@@ -1,1 +1,41 @@
 #!/bin/sh
+
+source "$(dirname "$0")/fancy.sh"
+
+STAGE_DIR="/tmp"
+STAGE_FILE="$STAGE_DIR/staged_files"
+
+function print_stage
+{
+    if [ -f "$STAGE_FILE" ]; then
+        cat "$STAGE_FILE"
+    else
+        displayWarning "The stage is empty"
+        exit 2
+    fi
+}
+
+function clear_stage
+{
+    rm -f "$STAGE_FILE"
+}
+
+
+if [ "$#" -eq 0 ]; then
+    print_stage
+fi
+
+case $1 in
+    clear|cl)
+        clear_stage
+        ;;
+    list|ls)
+        print_stage
+        ;;
+esac
+
+shift
+for arg in "$@"
+do
+    echo "$arg"
+done
